655d4799e5a65243051c7dfafedefae0e299ee49
JY_Han <qa7300@gmail.com>
2025-10-17 18:29:19 +0900

    refactor: SearchBar 아이콘을 public/icons/search.svg 파일로 변경
    - 인라인 SVG 제거
    - public/icons/search.svg 파일 사용
    - img 태그로 아이콘 로드
    - 불필요한 CSS color 설정 제거

--- src/commons/components/searchbar/index.tsx
+++ src/commons/components/searchbar/index.tsx
@@ -153,29 +153,11 @@ export const SearchBar = React.forwardRef<HTMLInputElement, SearchBarProps>(
       <div className={containerClassNames}>
         {showIcon && (
           <div className={styles.iconWrapper}>
-            <svg
+            <img
+              src="/icons/search.svg"
+              alt="search"
               className={styles.searchIcon}
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M21 21L16.65 16.65"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            />
           </div>
         )}
         <input
