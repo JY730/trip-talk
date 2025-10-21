f9b72e2c643c5b49ee53a49d223788dbf747a78f
JY_Han <qa7300@gmail.com>
2025-10-21 10:01:01 +0900

    feat: Pretendard 폰트를 기본 폰트로 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,9 @@ import { ThemeProvider } from "@/commons/providers/next-themes/next-themes.provi
 import { ReactQueryProvider } from "@/commons/providers/react-query/react-query.provider";
 import Layout from "@/commons/layout";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  variable: "--font-pretendard",
   weight: "100 900",
 });
 
@@ -28,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="ko" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${pretendard.variable} antialiased`}
       >
         <ReactQueryProvider>
           <ThemeProvider>
