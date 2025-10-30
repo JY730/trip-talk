088afcf024a2d2dd1eeb6296f11e73cb568434c1
JY_Han <qa7300@gmail.com>
2025-10-30 17:02:53 +0900

    chore(build): ignore TS build errors to unblock build per request

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
     webpack(config) {
         config.module = config.module || {};
         config.module.exprContextCritical = false;
